d879b7bdcb99e40aac5594d175f524287af41558
Scorpius-S <unknowncorp@live.co.uk>
2025-03-31 04:40:53 +0100

    Asteroids B: added constants.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 
 def main():
-	print("Starting Asteroids!")
+	print("Starting Asteroids!",
+	   "Screen width: 1280",
+	   "Screen height: 720")
 
 if __name__ == "__main__":
 	main()
+
+	from constants import *
