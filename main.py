564b10e0dd753a06fdaf9b610046ada2d39c783d
Scorpius-S <unknowncorp@live.co.uk>
2025-04-01 22:49:36 +0100

    Asteroids G: Added groups and containers to the game. The groups are used to group related objects together, while the containers are used to hold the objects. This allows for better organization and management of the game objects. The groups and containers are used to manage the game objects and their interactions.

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	
 	
 	print("Starting Asteroids!",
@@ -20,10 +24,10 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		
+		updatable.update(dt)
 		screen.fill((0, 0, 0))
-		player.draw(screen)
-		player.update(dt)
-
+		for drawable_sprite in drawable:
+			drawable_sprite.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
