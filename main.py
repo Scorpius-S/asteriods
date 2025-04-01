da90aed9f04031126f560c8e4dd6ca340934808f
Scorpius-S <unknowncorp@live.co.uk>
2025-04-01 09:44:00 +0100

    Asteroids D: Added FPS

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from constants import *
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	
 	
 	print("Starting Asteroids!",
 	   "Screen width: 1280",
@@ -18,6 +21,9 @@ def main():
 		screen.fill((0, 0, 0))
 
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000	
+		
+
 
 if __name__ == "__main__":
 	main()
