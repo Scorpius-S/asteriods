da3311b51a4368613066d6fa7b9b4ca0dbd39583
Scorpius-S <unknowncorp@live.co.uk>
2025-04-01 22:37:39 +0100

    Asteroids F: Added movement to the player ship

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_SPEED
 import pygame
 from circleshape import CircleShape
 
@@ -14,6 +14,23 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotation -= PLAYER_SPEED * dt
+        if keys[pygame.K_d]:
+            self.rotation += PLAYER_SPEED * dt
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
